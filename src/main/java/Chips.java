3a047d0767bbd7d663c0fb71d1a9aa3b68cc0459
zohinurmiah <zohinur.m17@gmail.com>
2026-05-26 22:34:27 -0400

    Worked on chips object so customer are allowed to order chips.

--- src/main/java/Chips.java
+++ src/main/java/Chips.java
@@ -1,2 +1,8 @@
-public class Chips {
+public class Chips implements MenuItem{
+    private final double price = 1.50;
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
 }
